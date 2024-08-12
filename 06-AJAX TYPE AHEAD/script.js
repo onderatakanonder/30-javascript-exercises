337a69de9a331e77500384bbfdd33f0ac2aea66c
Onder Atakan Onder <o.atakanonder@gmail.com>
2024-08-12 14:07:29 +0300

    6. ve 7. projeler eklendi

---
+++ 06-AJAX TYPE AHEAD/script.js
@@ -0,0 +1,47 @@
+
+const url = 'https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json'
+
+
+const cities = []
+
+
+fetch(url)
+    .then(res => res.json())
+    .then(data => cities.push(...data))
+
+
+
+function findMatches(wordToMatch, cities) {
+    return cities.filter(place => {
+        const regex = new RegExp(wordToMatch, 'gi')
+        return place.city.match(regex) || place.state.match(regex)
+    })
+}
+
+function numberWithCommas(x) {
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+}
+
+function displayMathes() {
+    const matchArray = findMatches(this.value, cities)
+    const html = matchArray.map(place => {
+        
+        const regex = new RegExp(this.value, 'gi')
+        const cityName = place.city.replace(regex, `<span class="hl">${this.value}</span>`)
+        const stateName = place.state.replace(regex, `<span class="hl">${this.value}</span>`)
+
+        return `
+        <li>
+            <span class="name" "hl"> ${cityName}, ${stateName}</span>
+            <span className="population"> ${numberWithCommas(place.population)}</span>
+        `
+    }).join('')
+
+    suggestions.innerHTML = html
+}
+
+const searchInput = document.querySelector('.search')
+const suggestions = document.querySelector('.suggestions')
+
+searchInput.addEventListener('change', displayMathes)
+searchInput.addEventListener('keyup', displayMathes)
